defef3e42a3fede059c91b434cf5c362ef09db77
gaurav <gauravshivmurat2@gmail.com>
2025-10-06 19:40:51 +0530

    Add Budget model and implement password hashing and token generation in User model

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
-
+import bcrypt from "bcrypt"
 const userSchema=new mongoose.Schema(
     {
         UserName:{
@@ -24,11 +24,49 @@ const userSchema=new mongoose.Schema(
         phone:{
             type :Number,
             required:true,
+        },
+        password:{
+            type:String,
+            required:true,
         }
         
 
     },{timestamps:true}
 );
+userSchema.pre("save",async function(next) {
+    if(!this.isModified("password"))return next();
+    console.log("Passwords recived",this.password)
+    this.password= await bcrypt.hash(this.password,10)
+    next()
+})
+userSchema.methods.isPasswordCorrect=async function (password) {
+    bcrypt.compare(password,this.password)
+}
+userSchema.methods.generateAcessToken=function(){
+    jwt.sign({
+        _id:this.id,
+        username:this.UserName,
+        email:this.email
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY,
+    },
+    )
+}
+userSchema.methods.generateRefreshToken=function(){
+    return jwt.sign({
+        _id:this.id,
+
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    },
+    )
+    
+}
+
 export default mongoose.model("User",userSchema);
 
 
