6a40aa3e370fbd0e5096849e86c414e2cd666cb5
gaurav <gauravshivmurat2@gmail.com>
2025-10-06 18:54:08 +0530

    Add Route model with schema definition for trip routes

--- backend/src/models/trip.model.js
+++ backend/src/models/trip.model.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import { Schema } from "mongoose";
+
 const tripSchema=new mongoose.Schema({
     user:{type:mongoose.Schema.Types.ObjectId,ref:"User",required:true},
     destination:{type:String,required:true},
