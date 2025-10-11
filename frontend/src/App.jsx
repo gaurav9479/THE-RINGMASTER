efa1a7f703ae0cecf9b67a2e002d39257d9fbe82
gaurav <gauravshivmurat2@gmail.com>
2025-10-11 19:02:13 +0530

    feat: add Tailwind CSS for styling and update App component
    - Added Tailwind CSS as a dependency in package.json.
    - Configured Tailwind CSS in a new tailwind.config.js file.
    - Updated App.css to include Tailwind's base, components, and utilities.
    - Refactored App.jsx to return a heading with updated text.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-    <h1>Sasta make mytrip</h1>
+  return <h1>SASTA MAKE-MY TRIP</h1>
 }
 
 export default App
