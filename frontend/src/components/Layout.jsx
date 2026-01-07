16594f5dca6b7a6732d33d6b5264016c9d48a536
Gaurav <gauravshivmurat2@gmail.com>
2026-01-07 16:03:25 +0530

    Navbar import issue resolved

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navbar from '../navbar/navbar';
+import Navbar from '../navbar/Navbar';
 import { Outlet } from 'react-router-dom';
 import LoginModal from './LoginModal';
 import RegisterModal from './RegisterModal';
